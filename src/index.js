5d8f89b07c19d03e589ac811f938263d9fe6d0b6
vshlg <varyasheleg14@gmail.com>
2019-03-16 21:22:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,47 @@
-// PLEASE DON'T change function name
+
+function divideSum(resultDivision,coinsValue,currency) { 
+    for (let propt in coinsValue) {
+        if (currency >= coinsValue[propt]) {
+            resultDivision[propt]+=1          
+            currency-=coinsValue[propt]
+            if (currency ==0) {
+                for (let propt in resultDivision) {
+                    if (resultDivision[propt]===0) {
+                        delete resultDivision[propt]
+                    }
+                }
+
+                return resultDivision;
+            }
+            return divideSum(resultDivision,coinsValue,currency);
+        }
+    }
+    
+}
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    console.log("start")
+    let coinsValue  = {}
+    if (currency ===0) {
+        return coinsValue;
+    }
+    else if (currency<0) {
+        return coinsValue;
+    }
+    else if (currency>10000) {
+        coinsValue.error="You are rich, my friend! We don't have so much coins for exchange"
+        return coinsValue;
+    }
+    else if (currency <= 10000) {
+        coinsValue = {
+            H : 50,
+            Q : 25,
+            D : 10,
+            N : 5,
+            P : 1.
+        }
+        let resultDivision = {H:0,Q:0,D:0,N:0,P:0.}
+        resultDivision = divideSum(resultDivision,coinsValue,currency)
+        return resultDivision;
+
+    }
 }
